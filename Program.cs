823a3988254d166ecb0538cca603b5233b995a55
github_carvalhio <raffael.carvallho@gmail.com>
2023-02-15 23:18:06 -0300

    First commit, project finished

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿try
+{
+    // GATHER IPUT FROM USER
+    Console.WriteLine("Por favor, digite uma frase para conveter em um acrônomo:");
+    String input = Console.ReadLine();
+
+    // OPTIONALS EXCLUSIONS
+    String exclusions = input.Replace("and", "");
+    String[] wordArr = exclusions.Split(' ');
+    Console.WriteLine(wordArr.Length);
+
+    // LOOP THROUGH SENTENCE
+    String acronym = "";
+    for (int i = 0; i < wordArr.Length; i++)
+    {
+        if (wordArr[i] != "")
+        {
+            acronym += wordArr[i][0];
+        }
+    }
+
+    acronym = acronym.ToUpper();
+    Console.WriteLine("Seu acrônomo é: " + acronym);
+
+
+
+} catch(Exception e)
+{
+    Console.WriteLine(e.ToString());
+}
